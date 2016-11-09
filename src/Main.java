1a444092348584a9f1b9f264a2bf6cd7eed4336e
Gr3mlin106 <maciej.matuszewski@me.com>
2016-11-09 23:15:00 +0100

    SMTP Server

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,125 @@
-s import java.io.*;
+import java.awt.HeadlessException;
+import java.io.*;
 import java.net.*;
+import java.sql.Timestamp;
 
 import javax.swing.JOptionPane;
+
 public class Main{
 	
     ServerSocket server;
     Socket client = null;
-    Socket client2 = null;
     ObjectOutputStream out;
-    ObjectOutputStream out2;
     ObjectInputStream in;
-    ObjectInputStream in2;
 
     void run()
     {
         try{
             server = new ServerSocket(9666, 10);
-            JOptionPane.showMessageDialog(null,"Serwer: Start");
+            System.out.println("Serwer: Start");
+//            JOptionPane.showMessageDialog(null,"Serwer: Start");
             client = server.accept();
-            JOptionPane.showMessageDialog(null,"Serwer: Connection 1 received from " + client.getInetAddress().getHostName());
-            client2 = server.accept();
-            JOptionPane.showMessageDialog(null,"Serwer: Connection 2 received from " + client2.getInetAddress().getHostName());
+            System.out.println("Serwer: Connection received from " + client.getInetAddress().getHostName());
+//            JOptionPane.showMessageDialog(null,"Serwer: Connection received from " + client.getInetAddress().getHostName());
             
             out = new ObjectOutputStream(client.getOutputStream());
-            out2 = new ObjectOutputStream(client2.getOutputStream());
             out.flush();
-            out2.flush();
             in = new ObjectInputStream(client.getInputStream());
-            in2 = new ObjectInputStream(client2.getInputStream());
+            String recivedMessage;
             
-            Thread t1 = new Thread(new Monitor(out, out2, in2, client2.getInetAddress().getHostName()));
-    		t1.start();
+            java.util.Date date= new java.util.Date();
+            System.out.println("Serwer ODS v0.0.1, " + new Timestamp(date.getTime()));
+            this.sendMessage("220", out);
             
-    		Thread t2 = new Thread(new Monitor(out2, out, in, client.getInetAddress().getHostName()));
-    		t2.start();
+            recivedMessage = (String)in.readObject();
+            if (!recivedMessage.contains("helo")){
+        		JOptionPane.showMessageDialog(null, "Serwer: Coś poszło nie tak :(");
+        		this.run();
+        		return;
+            }
+            
+            System.out.println("uzytkownik.internet.com Hello at serwer.email.com [" + client.getInetAddress() + "]");
+            this.sendMessage("250", out);
+            
+            recivedMessage = (String)in.readObject();
+            if (!recivedMessage.contains("mail from")){
+        		JOptionPane.showMessageDialog(null, "Serwer: Coś poszło nie tak :(");
+        		this.run();
+        		return;
+            }
+            
+            String from = recivedMessage.substring(12, recivedMessage.length() - 1);
+            System.out.println("OK");
+            this.sendMessage("250", out);
+
+            
+            recivedMessage = (String)in.readObject();
+            if (!recivedMessage.contains("rcpt to")){
+        		JOptionPane.showMessageDialog(null, "Serwer: Coś poszło nie tak :(");
+        		this.run();
+        		return;
+            }
+            
+            String to = recivedMessage.substring(10, recivedMessage.length() - 1);
+            System.out.println("Accepted");
+            this.sendMessage("250", out);
+
+            
+            recivedMessage = (String)in.readObject();
+            if (!recivedMessage.contains("data")){
+        		JOptionPane.showMessageDialog(null, "Serwer: Coś poszło nie tak :(");
+        		this.run();
+        		return;
+            }
+            
+            System.out.println("Enter message");
+            this.sendMessage("354", out);
+            
+            recivedMessage = (String)in.readObject();
+            
+            System.out.println("\nFrom: " + from + "\nTo: " + to + "\nData: " + recivedMessage + "\n");
+            
+            System.out.println("OK id=1Coql6-0003Qi-MP");
+            this.sendMessage("250", out);
+
+            
+            recivedMessage = (String)in.readObject();
+            if (!recivedMessage.contains("quit")){
+        		JOptionPane.showMessageDialog(null, "Serwer: Coś poszło nie tak :(");
+        		this.run();
+        		return;
+            }
+            
+            System.out.println("serwer.email.com closing connection");
+            this.sendMessage("221", out);
+            
     		
         }
         catch(IOException ioException){
             ioException.printStackTrace();
-        }
+        } catch (HeadlessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
         finally{
         }
     }
-
-    public static void main(String args[])
+	
+	void sendMessage(String msg, ObjectOutputStream out)
     {
+        try{
+        	System.out.println("Server -> " + client + ": "+msg);
+            out.writeObject(msg);
+            out.flush();
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
         Main server = new Main();
-        server.run();
-        
+        server.run();  
     }
 }
